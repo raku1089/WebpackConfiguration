eed66cb6ef690d05d34ddd7cd8a24a32ddea83f0
Rakesh Kumar <rakesh10kumar89@gmail.com>
2019-05-15 01:33:34 +0530

    webpack configuration , first time setup from basic

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/main.css';
+import { printHello, hey } from './js/module.js';
+console.log('Hello webpack configuration ..');
+printHello();
+hey();
---
+++ src/js/module.js
@@ -0,0 +1,8 @@
+function printHello() {
+  console.log('Hello js folder js file printhello()');
+}
+
+function hey() {
+  console.log('Hello js folder Hey() ..');
+}
+export { printHello, hey };
